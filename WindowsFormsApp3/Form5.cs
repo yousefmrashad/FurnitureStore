5b40c0f1125d877181be142d4b03116999330970
YousefRashad <yousefrashad4444@yahoo.com>
2022-05-30 17:21:52 +0200

    Semantics.

--- WindowsFormsApp3/Form5.cs
+++ WindowsFormsApp3/Form5.cs
@@ -69,20 +69,39 @@ namespace WindowsFormsApp3
             Application.Exit();
         }
 
-        private void dataGridView3_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            try
-            {
-                int index = e.RowIndex;
-                DataGridViewRow selectedRow = dataGridView3.Rows[index];
-                textBox2.Text = selectedRow.Cells[1].Value.ToString();
-                textBox4.Text = selectedRow.Cells[3].Value.ToString();
-                ProdID = int.Parse(selectedRow.Cells[0].Value.ToString());
-            }
-            catch
-            {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
 
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string doc = "";
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                doc += "Receipt ID: " + dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                cmd = new SqlCommand($"SELECT SellerName FROM Sellers WHERE SellerID = {int.Parse(dataGridView1.SelectedRows[0].Cells[1].Value.ToString())}", conn);
+                dr = cmd.ExecuteReader();
+                while(dr.Read())
+                {
+                    doc += "\n" + "Seller ID: " + dr[0].ToString();
+                }
+                doc += "\n" + dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                doc += "\n" + dataGridView1.SelectedRows[0].Cells[3].Value.ToString() + "LE";
             }
+            e.Graphics.DrawString(doc, new Font("Times New Roman", 36, FontStyle.Bold), Brushes.Black, new Point(25, 25));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Enabled = true;
+            cmd = new SqlCommand($"UPDATE Receipts SET ReceiptTotal = ReceiptTotal + {CurrentTotal} WHERE ReceiptID = {int.Parse(textBox1.Text)}", conn);
+            cmd.ExecuteNonQuery();
+            refresh();
+            CurrentTotal = 0;
+            label9.Text = $"Total is: {CurrentTotal} LE";
+            clear();
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -136,47 +155,6 @@ namespace WindowsFormsApp3
             clear();
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            printPreviewDialog1.Document = printDocument1;
-            printPreviewDialog1.ShowDialog();
-        }
-
-        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-        {
-            string doc = "";
-            if (dataGridView1.SelectedRows.Count > 0)
-            {
-                doc += "Receipt ID: " + dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
-                cmd = new SqlCommand($"SELECT SellerName FROM Sellers WHERE SellerID = {int.Parse(dataGridView1.SelectedRows[0].Cells[1].Value.ToString())}", conn);
-                dr = cmd.ExecuteReader();
-                while(dr.Read())
-                {
-                    doc += "\n" + "Seller ID: " + dr[0].ToString();
-                }
-                doc += "\n" + dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
-                doc += "\n" + dataGridView1.SelectedRows[0].Cells[3].Value.ToString() + "LE";
-            }
-            e.Graphics.DrawString(doc, new Font("Times New Roman", 36, FontStyle.Bold), Brushes.Black, new Point(25, 25));
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            dataGridView1.Enabled = true;
-            cmd = new SqlCommand($"UPDATE Receipts SET ReceiptTotal = ReceiptTotal + {CurrentTotal} WHERE ReceiptID = {int.Parse(textBox1.Text)}", conn);
-            cmd.ExecuteNonQuery();
-            refresh();
-            CurrentTotal = 0;
-            label9.Text = $"Total is: {CurrentTotal} LE";
-            clear();
-        }
-        void clear()
-        {
-            textBox2.Clear();
-            textBox3.Clear();
-            textBox4.Clear();
-        }
-
         private void button7_Click(object sender, EventArgs e)
         {
             textBox1.Text = ReceiptNo.ToString();
@@ -184,6 +162,58 @@ namespace WindowsFormsApp3
             cmd.ExecuteNonQuery();
             refresh();
             ReceiptNo++;
+        }
+        
+        private void button8_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int index = e.RowIndex;
+                DataGridViewRow selectedRow = dataGridView1.Rows[index];
+
+                int i;
+                i = int.Parse(selectedRow.Cells[0].Value.ToString());
+                textBox1.Text = i.ToString();
+                cmd = new SqlCommand($"SELECT ProdID, Qty FROM ReceiptDetails WHERE ReceiptID = {i}", conn);
+                cmd.ExecuteNonQuery();
+                ds.Tables.Remove("ReceiptDetails");
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "ReceiptDetails");
+                dataGridView2.DataSource = ds.Tables["ReceiptDetails"];
+                dataGridView2.Refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView3_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int index = e.RowIndex;
+                DataGridViewRow selectedRow = dataGridView3.Rows[index];
+                textBox2.Text = selectedRow.Cells[1].Value.ToString();
+                textBox4.Text = selectedRow.Cells[3].Value.ToString();
+                ProdID = int.Parse(selectedRow.Cells[0].Value.ToString());
+            }
+            catch
+            {
+
+            }
+        }
+
+        void clear()
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
         }
 
         void refresh()
@@ -218,35 +248,6 @@ namespace WindowsFormsApp3
             {
                 comboBox1.Items.Add(dr["CatName"].ToString());
             }
-        }
-
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            try
-            {
-                int index = e.RowIndex;
-                DataGridViewRow selectedRow = dataGridView1.Rows[index];
-
-                int i;
-                i = int.Parse(selectedRow.Cells[0].Value.ToString());
-                textBox1.Text = i.ToString();
-                cmd = new SqlCommand($"SELECT ProdID, Qty FROM ReceiptDetails WHERE ReceiptID = {i}", conn);
-                cmd.ExecuteNonQuery();
-                ds.Tables.Remove("ReceiptDetails");
-                da = new SqlDataAdapter(cmd);
-                da.Fill(ds, "ReceiptDetails");
-                dataGridView2.DataSource = ds.Tables["ReceiptDetails"];
-                dataGridView2.Refresh();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            refresh();
         }
     }
 }
